ca622ea5718ccec0eb5c3078bffd36b9f7b3da0e
Coffee <zjchen1234@foxmail.com>
2025-08-29 15:00:07 +0800

    fix: server crashes when log middleware throws error (#41)

--- src/server/middlewares/log.ts
+++ src/server/middlewares/log.ts
@@ -2,8 +2,6 @@ import { RequestHandler } from 'express';
 import morgan from 'morgan';
 import dayjs from 'dayjs';
 import { addColor, getHttpStatusColor } from '../../utils/color';
-import { createOpenAiHttpError } from '../../utils/error';
-import { STATUS_CODES } from '../../utils/consts';
 
 export function createIncomingLogMiddleware({
   isDev = true,
@@ -12,23 +10,9 @@ export function createIncomingLogMiddleware({
     (tokens, req, res) => {
       const timestamp = dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
 
-      const method = tokens['method']?.(req, res);
+      const method = tokens['method']?.(req, res) ?? '';
 
-      if (!method) {
-        throw createOpenAiHttpError({
-          status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-          message: `Incoming log token 'method' not found`,
-        });
-      }
-
-      const url = tokens['url']?.(req, res);
-
-      if (!url) {
-        throw createOpenAiHttpError({
-          status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-          message: `Incoming log token 'url' not found`,
-        });
-      }
+      const url = tokens['url']?.(req, res) ?? '';
 
       return [
         addColor(`[${timestamp}]`, 'gray', isDev),
@@ -52,43 +36,15 @@ export function createOutgoingLogMiddleware({
   return morgan((tokens, req, res) => {
     const timestamp = dayjs().format('YYYY-MM-DD HH:mm:ss.SSS');
 
-    const method = tokens['method']?.(req, res);
+    const method = tokens['method']?.(req, res) ?? '';
 
-    if (!method) {
-      throw createOpenAiHttpError({
-        status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-        message: `Outgoing log token 'method' not found`,
-      });
-    }
+    const url = tokens['url']?.(req, res) ?? '';
 
-    const url = tokens['url']?.(req, res);
+    const status = tokens['status']?.(req, res) ?? '';
 
-    if (!url) {
-      throw createOpenAiHttpError({
-        status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-        message: `Outgoing log token 'url' not found`,
-      });
-    }
+    const statusColor = status ? getHttpStatusColor(Number(status)) : 'gray';
 
-    const status = tokens['status']?.(req, res);
-
-    if (!status) {
-      throw createOpenAiHttpError({
-        status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-        message: `Outgoing log token 'status' not found`,
-      });
-    }
-
-    const statusColor = getHttpStatusColor(Number(status));
-
-    const responseTime = tokens['response-time']?.(req, res);
-
-    if (!responseTime) {
-      throw createOpenAiHttpError({
-        status: STATUS_CODES.INTERNAL_SERVER_ERROR,
-        message: `Outgoing log token 'response-time' not found`,
-      });
-    }
+    const responseTime = tokens['response-time']?.(req, res) ?? '';
 
     return [
       addColor(`[${timestamp}]`, 'gray', isDev),
